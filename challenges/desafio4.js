c80a2e635baf15079487b0a74b68199bd8c792a1
Joao Soutto <joaosoutto@hotmail.com>
2020-09-12 19:51:40 -0300

    ctrl shift i pra cima deles

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,3 +1,6 @@
-db.produtos.updateOne({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: { $type: "date" }}});
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $currentDate: { ultimaModificacao: { $type: "date" } } }
+);
 
-db.produtos.find({ ultimaModificacao: { $exists: true }}, { _id: 0, nome: 1 });
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
