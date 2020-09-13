64bcc596bd6396c4f3a109585445317e4dcf3146
Filipe de Castro <filipeyoga@gmail.com>
2020-09-13 07:22:29 -0300

    start: Initial commit

---
+++ desafio1.js
@@ -0,0 +1,3 @@
+db.produtos.update({}, { $set: { criadoPor: "Ronald McDonald" } }, false, true);
+
+db.produtos.find({},{ _id: false, nome: true, criadoPor: true }).pretty();
---
+++ desafio7.js
@@ -0,0 +1,18 @@
+// Crie uma query que faça a remoção do item cebola em todos os sanduíches.
+
+db.produtos.updateMany(
+  {},
+  {
+    $pull: {
+      ingredientes: "cebola",
+    },
+  }
+);
+
+// Crie uma query que retorne o nome e ingredientes de todos os documentos.
+
+db.produtos.find(
+    { },
+    { _id: false, nome: true, ingredientes: true }
+  ).pretty();
+  
