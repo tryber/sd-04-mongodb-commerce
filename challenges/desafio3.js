11d7fef699a3bfb1c19af24130e7e10aeb427776
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-09-13 22:37:45 -0300

    desafio 7 - 8

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,4 +1,4 @@
-db.produtos.updateMany({}, { $set: { avaliacao: NumberInt(1) } });
+db.produtos.updateMany({}, { $set: { avaliacao: NumberInt(0) } });
 
 db.produtos.updateMany({ tags: "bovino" }, { $inc: { avaliacao: 5 } });
 
