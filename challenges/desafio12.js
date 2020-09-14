8c5e9f138dc1191734a7d9aa6897cf7d232adc11
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-09-14 18:37:34 -0300

    desafio11 e 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,2 +1,2 @@
-db.produtos.updateMany({}, {$push: {"valoresNutricionais": {$each: [], $sort: {"valoresNutricionais.percentual": -1}}}});
-db.produtos.find({}, {"nome": 1, "valoresNutricionais.percentual": 1, "_id": 0});
+db.produtos.updateMany({}, {$push: {"valoresNutricionais": {$each: [], $sort: {"percentual": -1}}}});
+db.produtos.find({}, {"nome": 1, "valoresNutricionais": 1, "_id": 0});
