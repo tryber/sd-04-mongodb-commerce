29ec70c8bd189457d89612bb53c28644bb4180a6
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-09-13 22:17:43 -0300

    desafio 4

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,7 +1,7 @@
-db.produtos.updateMany({}, {avaliacao: NumberInt(1)});
+db.produtos.updateMany({}, { avaliacao: NumberInt(1) });
 
-db.produtos.updateMany({tags: "bovino"}, {avaliacao: 5});
+db.produtos.updateMany({ tags: "bovino" }, { avaliacao: 5 });
 
-db.produtos.updateMany({tags: "ave"}, {avaliacao: 3});
+db.produtos.updateMany({ tags: "ave" }, { avaliacao: 3 });
 
 db.produtos.find({}, { nome: 1, avaliacao: 1, _id: 0 });
