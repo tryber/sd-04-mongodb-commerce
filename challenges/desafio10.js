b2212990204972cf27d8c05a29c4c206878640f8
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-09-13 22:58:26 -0300

    desafio 11

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,7 +1,7 @@
-db.produtos.updateMany({}, {vendasPorDia: [0, 0, 0, 0, 0, 0, 0]});
+db.produtos.updateMany({}, { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } } });
 
-db.produtos.updateOne({nome: "Big Mac"}, {$inc: {"vendasPorDia.3": 60}});
+db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
 
-db.produtos.updateMany({tags: {$all: ["bovino", "pão"]}}, {$inc: {"vendasPorDia.6": 120}});
+db.produtos.updateMany({ tags: { $all: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.6": 120 } });
 
 db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 });
