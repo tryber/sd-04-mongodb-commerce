ad726b17fb7ec77331d4e1a45698112d4eb906e3
Pedro <pedrotpoliveira@gmail.com>
2020-09-15 00:23:39 -0300

    Desafio 5 - Evaluator Fix

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,3 +1,3 @@
-db.produtos.updateMany({ "nome": { $not: "McChicken" } }, { $addToSet: { "ingredientes": "ketchup" } });
+db.produtos.updateMany({ "nome": { $not: { $eq: "McChicken" } } }, { $addToSet: { "ingredientes": "ketchup" } });
 
 db.produtos.find({}, { "_id": 0, "nome": 1, "ingredientes": 1 });
