4b62c4bb7d5ea025f02e099b516e443a1ef5bd34
Caio Ribeiro <cacoribeiro@gmail.com>
2020-09-15 14:40:39 -0300

    Desafio 15

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,7 +1,9 @@
 db.produtos.updateMany(
   {
-      valoresNutricionais: {$elemMatch : {tipo: "sódio", percentual: {$gt: 40} }}
-  }
+    valoresNutricionais: {
+      $elemMatch: { tipo: "sódio", percentual: { $gt: 40 } },
+    },
+  },
   { $push: { tags: "muito sódio" } }
 );
 
