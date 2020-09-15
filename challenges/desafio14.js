4ece21bf912244594a8d20b1c43e4850875bc999
Tiago-ers <trsilva284@gmail.com>
2020-09-15 09:58:07 -0300

    cc

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,15 +1,14 @@
 db.produtos.updateMany(
-    {
-      "valoresNutricionais": {
-        $elemMatch: { "tipo": "sódio", "percentual": { $gt: 20, $lt: 40 } },
-      },
+  {
+    "valoresNutricionais": {
+      $elemMatch: { "tipo": "sódio", "percentual": { $gt: 20, $lt: 40 } },
     },
-    {
-      $push: {
-        "tags": { $each: ["contém sódio"] },
-      },
-    }
-  );
-  
-  db.produtos.find({}, { "_id": 0, "nome": 1, "tags": 1 });
-  
+  },
+  {
+    $push: {
+      "tags": { $each: ["contém sódio"] },
+    },
+  }
+);
+
+db.produtos.find({}, { "_id": 0, "nome": 1, "tags": 1 });
