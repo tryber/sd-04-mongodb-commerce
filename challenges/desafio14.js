3e9ca8ed7c2f17789298ae0f8c5c7c983dec6b93
Julio Cesar Tinin <juliocesartinin@pop-os.localdomain>
2020-09-16 19:14:57 -0300

    No bad energy Please No bad ENERGY

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany({ valoresNutricionais:
-{$elemMatch: {$in: [{percentual: {$gte:20, $lte:40}, "tipo": "sódio" }]}}},
+{$elemMatch: {$in: [{percentual: {$gt:20, $lt:40}, "tipo": "sódio" }]}}},
 {$push: {tags: "contém sódio"}});
 
 db.produtos.find({}, {nome:1, tags:1, _id:0}).pretty();
