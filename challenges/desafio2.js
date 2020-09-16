29fb658ccfcffd29c7da0dc3abd445050e0f5807
Kaecio <kaecio@yahoo.com.br>
2020-09-16 18:30:25 -0300

    Desafio 2, mudando a sintaxe

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany({},{$set:{"valorUnitario":NumberDecimal("0.00")}});
-db.produtos.find({"criadoPor":"Ronald McDonald"},{"nome":1,"valorUnitario":1,"_id":0});
+db.produtos.find({},{"nome":1,"valorUnitario":1,"_id":0});
 /*Desafio 2
 Inclua o campo valorUnitario em todos os documentos em que esse campo não existe e 
 atribua a ele o valor "0.00", com o tipo NumberDecimal.
