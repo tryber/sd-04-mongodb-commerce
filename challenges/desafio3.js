b1530738cbb3013fb8a9d11c3b95176f669fee81
danielpantalena <60634194+danielPantalena@users.noreply.github.com>
2020-09-16 17:01:40 -0300

    Update challenges/desafio3.js
    Co-authored-by: Leandro Marten <62161776+leandromarten1@users.noreply.github.com>

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,7 +1,7 @@
 db.produtos.updateMany({}, { $set: { avaliacao: NumberInt(0) } });
 
 db.produtos.updateMany(
-  { tags: { $all: ["bovino"] } },
+  { tags: "bovino"} },
   { $inc: { avaliacao: 5 } }
 );
 
