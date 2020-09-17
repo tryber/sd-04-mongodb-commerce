3e3bbb28fa63c3cfc118adcac336bb3c5a847e6a
djonata-dias <diasdjonata@gmail.com>
2020-09-17 20:36:47 -0300

    Desafio 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -3,7 +3,7 @@ db.produtos.updateMany(
         "valoresNutricionais": { $elemMatch: { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } } }
     },
     {
-        $push: { "tags": "contém sódio" },
+        $push: { tags: "contém sódio" },
     }
 );
 
