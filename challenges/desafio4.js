9f8e0f9b69e21429f54220a9431517db2ec44871
Lucas Ribas <lucas@mundoribas.com.br>
2020-09-17 16:28:27 -0300

    Desafio 7 - ok

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -4,7 +4,5 @@ db.produtos.updateOne(
 );
 
 db.produtos
-  .find(
-    { ultimaModificacao: { $exists: true } },
-    { _id: 0, nome: 1 }
-  ).pretty();
+  .find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 })
+  .pretty();
