7b85f886ae35c00f96b5f9d4d9a638b8421bb7e8
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-09-17 15:32:33 -0300

    desafios 10 ao 15

---
+++ challenges/desafio10.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+  {},
+  { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } } }
+);
+
+db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
+
+db.produtos.updateOne(
+  { nome: "Big Mac", tags: { $in: ["pão", "bovino"] } },
+  { $inc: { "vendasPorDia.6": 120 } }
+);
+
+db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 });
