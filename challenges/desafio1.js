b07f8e81a7e203d3ff1f1ec7bb3c4e14ffc4ed00
Lucas Ribas <lucas@mundoribas.com.br>
2020-09-17 16:35:58 -0300

    Desafio 9 - ok

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,9 +1,5 @@
-db.produtos.updateMany(
-  {},
-  { $set: { criadoPor: "Ronald MacDonald" } }
-);
+db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDonald" } });
 
-db.produtos.find(
-  {},
-  { _id: 0, nome: 1, criadoPor: 1 }
-).pretty();
+db.produtos
+  .find({ criadoPor: "Ronald McDonald" }, { _id: 0, nome: 1, criadoPor: 1 })
+  .pretty();
