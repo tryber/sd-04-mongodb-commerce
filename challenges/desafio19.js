20368ff8af2bc2774b696dda959e5832212daa7d
Thiago Tavares <thiagooo15@gmail.com>
2020-09-17 19:36:11 -0300

    ccfix

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -1,4 +1,4 @@
 // Renomeie o campo descricao para descricaoSite em todos os documentos.
-// Para isso, escreva no arquivo desafio19.js duas queries, nesta ordem:
+// Para isso, escreva no duas queries, nesta ordem:
 // Crie uma query que faça a renomeação do campo descricao para descricaoSite em todos os documentos.
 // Crie uma query que retorne o nome, descricao e descricaoSite de todos os documentos.
