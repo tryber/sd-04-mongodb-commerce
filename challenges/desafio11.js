5f20423e5283bd579f9843ce3ca697bd730cd1b3
djonata-dias <diasdjonata@gmail.com>
2020-09-17 18:45:58 -0300

    Desafio 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+    {},
+    {
+        $push: {
+            "tags": {
+                $each: ["combo", "tasty"],
+                $sort: { tags: 1 }
+            }
+        },
+    }
+);
+
+db.produtos.find({}, { _id: false, nome: 1, tags: true });
