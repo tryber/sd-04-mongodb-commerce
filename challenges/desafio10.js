781075a352aa6eb6bad30a2d162b14dea6242e56
Ronald Lima <ronaldazevedolima@gmail.com>
2020-09-17 11:21:33 -0300

    sem ideias

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -3,5 +3,5 @@ db.produtos.updateMany({}, {
     vendasPorDia: {
       $each: [0, 0, 0, 0, 0, 0, 0]}}});
 db.produtos.updateOne({nome: "Big Mac"}, {$inc: {"vendasPorDia.3":60}});
-db.produtos.updateMany({$or: [{tags: "bovino"}, {tags: "pão"}]}, {$inc: {"vendasPorDia.7":120}});
+db.produtos.updateMany({$or: [{tags: "bovino"}, {tags: "pão"}]}, {$inc: {"vendasPorDia.6":120}});
 db.produtos.find({}, {_id: 0, nome: 1, vendasPorDia: 1});
