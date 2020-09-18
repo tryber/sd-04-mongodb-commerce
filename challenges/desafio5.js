d98999ea094f6a65d7c621d5b8f62ba2fb52803b
lourenco <loulage@gmail.com>
2020-09-18 15:50:43 -0300

    desaio5

---
+++ challenges/desafio5.js
@@ -0,0 +1,10 @@
+db.produtos.updateMany(
+    {
+        nome: { $ne: "McChicken" }
+    },
+    {
+        $addToSet: { ingredientes: "ketchup" }
+    }
+);
+
+db.produtos.find({}, {_id: false, nome: true, ingredientes: true}).pretty();
