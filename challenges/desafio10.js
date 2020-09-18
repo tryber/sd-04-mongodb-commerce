917c1186ee047fb80553053595da9522a51949ac
Thiago Tavares <thiagooo15@gmail.com>
2020-09-18 12:48:04 -0300

    requisito 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -8,4 +8,10 @@
 // 2. Crie uma query que incremente as vendas de Big Mac às quartas-feiras em 60.
 // 3. Crie uma query que incremente as vendas de todos os sanduíches de carne do tipo bovino e pão aos sábados em 120.
 // 4. Crie uma query que retorne o nome e vendasPorDia de todos os documentos.
-db.produtos
+db.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
+db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
+db.produtos.updateMany(
+  { tags: { $in: ["bovino", "pão"] } },
+  { $inc: { "vendasPorDia.6": 120 } }
+);
+db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });
