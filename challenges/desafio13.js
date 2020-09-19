ddc12b959b2b887e7a8873950978721d2b985fe0
Kaecio <kaecio@yahoo.com.br>
2020-09-19 01:35:25 -0300

    Desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,8 @@
+db.produtos.updateMany({"valoresNutricionais.percentual":{$gte:40}},{$addToSet:{"tags":"muito sódio"}});
+db.produtos.find({},{"nome":1,"tags":1,"_id":0});
+/*Adicione o elemento muito sódio ao final do array tags nos produtos 
+em que o percentual de sódio seja maior ou igual a 40.
+Para isso, escreva no arquivo desafio13.js duas queries, nesta ordem:
+Crie uma query que faça a adição do elemento muito sódio ao 
+final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40.
+Crie uma query que retorne o nome e tags de todos os documentos. */
