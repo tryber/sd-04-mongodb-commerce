b16f20e6347884bbc2343f0d64fdfcf5daebb085
Pedro <pedrotpoliveira@gmail.com>
2020-09-20 12:34:50 -0300

    Desafio 10 - Refactoring

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,6 +2,6 @@ db.produtos.updateMany({}, { $push: { "vendasPorDia": { $each: [0, 0, 0, 0, 0, 0
 
 db.produtos.updateMany({ "nome": "Big Mac" }, { $set: { "vendasPorDia.3": 60 } });
 
-db.produtos.updateMany({ $and: [{ "tags": "bovino" }, { "tags": "pão" }] }, { $set: { "vendasPorDia.6": 120 } });
+db.produtos.updateMany({ "tags": { $all: ["bovino", "pão"] } }, { $set: { "vendasPorDia.6": 120 } });
 
 db.produtos.find({}, { "_id": 0, "nome": 1, "vendasPorDia": 1 });
