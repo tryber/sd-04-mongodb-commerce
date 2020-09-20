57adf3af3a64d63eecd6a28f8d1a2f415f505856
Kaecio <kaecio@yahoo.com.br>
2020-09-20 19:38:46 -0300

    Desafio 14, e  corrigidos

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,4 +1,4 @@
-db.produtos.updateMany({"valoresNutricionais.2.percentual":{$lt:20,$gt:40}},{$push:{"tags":"contém sódio"}});
+db.produtos.updateMany({"valoresNutricionais.2.percentual":{$gt:20,$lt:40}},{$push:{"tags":"contém sódio"}});
 db.produtos.find({},{"nome":1,"tags":1,"_id":0}).pretty();
 
 /*Desafio 14
