f1a3225c61887f26c39143f0c036e89dfe6b592e
Kaecio <kaecio@yahoo.com.br>
2020-09-20 19:32:13 -0300

    Desafio 14,trocando muito por contem sodio

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,4 +1,4 @@
-db.produtos.updateMany({"valoresNutricionais.2.percentual":{$lt:20,$gt:40}},{$push:{"tags":"muito sódio"}});
+db.produtos.updateMany({"valoresNutricionais.2.percentual":{$lt:20,$gt:40}},{$push:{"tags":"contém sódio"}});
 db.produtos.find({},{"nome":1,"tags":1,"_id":0}).pretty();
 
 /*Desafio 14
