1ca4904d13e91cec630f0425c2980a66205eea12
Kaecio <kaecio@yahoo.com.br>
2020-09-20 20:43:07 -0300

    Desafio 17, corrigindo de novo

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,4 +1,4 @@
-db.prosutos.createIndex({"descricao":"text"},{default_language:"portuguese"});
+db.produtos.createIndex({"descricao":"text"},{default_language:"portuguese"});
 db.produtos.find({$text:{$search:"frango hamburguer"}}).count();
 /*Desafio 17
 Conte quantos documentos contêm as palavras frango e hamburguer utilizando o operador $text.
